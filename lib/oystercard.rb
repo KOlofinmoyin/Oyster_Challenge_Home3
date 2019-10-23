2362559299a66dfd47cd4498e6cb3fdb3d566e94
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2019-10-23 06:45:19 +0100

    User story 4: (delivered) I need my fare deducted from my card

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,19 @@
 
 class Oystercard
   attr_reader :balance
+  MAXIMUM_BALANCE = 90
 
   def initialize
     @balance = 0
   end
 
   def top_up(amount)
+    fail "Maximum balance of £#{MAXIMUM_BALANCE} exceeded." if @balance + amount > MAXIMUM_BALANCE
     @balance += amount
   end
+
+  def deduct(amount)
+    @balance -= amount
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,15 +1,31 @@
 require 'oystercard'
 
 describe Oystercard do
-  it "returns balance of 0" do
-    expect(subject.balance).to eq 0
-  end
 
-  it { is_expected.to respond_to(:top_up).with(1).argument }
-
-describe "#top_up" do
-  it "adds one to my card balance" do
-    expect { subject.top_up 1}.to change{ subject.balance}.by 1
-  end
+it "returns a balance of zero on my card" do
+  expect(subject.balance).to eq 0
 end
+
+# In order to keep using public transport
+# As a customer
+# I want to add money to my card
+it { is_expected.to respond_to(:top_up).with(1).argument}
+
+it "adds some money to my card" do
+  expect { subject.top_up(1) }.to change { subject.balance }.by 1
+end
+
+it "raises an error when I topup over the maximum allowable balance" do
+  maximum_balance = Oystercard::MAXIMUM_BALANCE
+  subject.top_up maximum_balance
+  expect{ subject.top_up 1 }.to raise_error "Maximum balance of £#{maximum_balance} exceeded."
+end
+
+# In order to pay for my journey
+# As a customer
+# I need my fare deducted from my card
+it "deducts a fare from my card" do
+  expect { subject.deduct 1 }.to change{ subject.balance }.by(-1)
+end
+
 end

f7f9bb07b58121b69ed3c69b392b4c8acdd93c3e
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2019-10-23 01:25:21 +0100

    User story 2: delivered I want to add money to my card.

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,7 +1,15 @@
 require 'oystercard'
 
 describe Oystercard do
-  it "returns zero balance" do
+  it "returns balance of 0" do
     expect(subject.balance).to eq 0
   end
+
+  it { is_expected.to respond_to(:top_up).with(1).argument }
+
+describe "#top_up" do
+  it "adds one to my card balance" do
+    expect { subject.top_up 1}.to change{ subject.balance}.by 1
+  end
+end
 end
